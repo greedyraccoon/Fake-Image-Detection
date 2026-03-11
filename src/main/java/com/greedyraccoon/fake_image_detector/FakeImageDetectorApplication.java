1dc8e11ffa720dfaf3f1a7f19f7d93b4c4f1d99d
SahilDange14 <sahildange1410@gmail.com>
2026-03-11 23:31:55 +0530

    defined image detection service

--- src/main/java/com/greedyraccoon/fake_image_detector/FakeImageDetectorApplication.java
+++ src/main/java/com/greedyraccoon/fake_image_detector/FakeImageDetectorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FakeImageDetectorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FakeImageDetectorApplication.class, args);
 	}
--- src/main/java/com/greedyraccoon/fake_image_detector/service/ImageDetectionService.java
+++ src/main/java/com/greedyraccoon/fake_image_detector/service/ImageDetectionService.java
@@ -1,5 +1,41 @@
 package com.greedyraccoon.fake_image_detector.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public class ImageDetectionService {
-    
+
+    @Value("${huggingface.api.key}")
+    private String apiKey;
+
+    @Value("${huggingface.api.url}")
+    private String apiUrl;
+
+    public String analyzeImage(MultipartFile file) throws IOException, InterruptedException {
+        byte[] imageBytes = file.getBytes();
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .header("Authorization", apiKey)
+                .header("Content-Type", "application/octet-stream")
+                .POST(HttpRequest.BodyPublishers.ofByteArray(imageBytes))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("API Error: " + response.body());
+        }
+
+        return response.body();
+    }
 }
